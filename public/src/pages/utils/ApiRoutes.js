ceb22c8b0d9084867421db1230b402f96ad9bc5f
PranavGaur7 <raghavgau7@gmail.com>
2024-02-25 17:48:28 +0530

    yoo

--- public/src/pages/utils/ApiRoutes.js
+++ public/src/pages/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000"
+export const host = "https://chat-app-server-nine-kappa.vercel.app"
 export const registerRoutes = `${host}/user/register`
 export const loginRoutes = `${host}/user/login`
 export const getUserRoutes = `${host}/user/getUser`
