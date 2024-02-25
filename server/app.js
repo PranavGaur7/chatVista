974c41f0544e95d865518f86d0e9c20534ee48d2
PranavGaur7 <raghavgau7@gmail.com>
2024-02-25 17:55:04 +0530

    vercel app

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const socket = require('socket.io')
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://chatvista-jet.vercel.app/"],
     methods: ["GET", "POST"],
     credentials: true,
 }))
@@ -20,7 +20,7 @@ app.use(cookieParser())
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://chatvista-jet.vercel.app/",
         credentials: true,
     },
 })
